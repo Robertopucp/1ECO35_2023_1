0377956ca3083491c68056f8b0fc5863268e6f10
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-04-07 10:21:58 -0500

    Modificación R

--- Labs/tarea1/Grupo_11_r.R
+++ Labs/tarea1/Grupo_11_r.R
@@ -1,3 +1,24 @@
-#Integrantes
-#
-#
+# Integrantes ####
+# Kevin Pareja (20196318)
+# Elian Tongombol (20196453)
+# Paola Aranda (20196052)
+# María Alejandra Colán (20190515)
+
+# Consideraciones previas: #####
+## Borrando el environment ####
+rm(list = ls())
+
+## Borrando los gráficos ####
+graphics.off()
+
+##Borrando la consola ####
+cat("\014")
+
+# Desarrollo de las preguntas ####
+
+## Pregunta 1: Paste #### 
+# Use paste o paste0 para crear la siguiente variable de texto: "Facultad de Ciencias Sociales \ 2023"
+
+## Pregunta 2: Operador pip %>% ####
+##Use el operador %>% para tomar el valor absoluto de -3.1416 , luego elevar al cuadrado, tomar logaritmo y convertir a número entero.
+
