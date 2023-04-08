02844dfb88d35678f89cbc9bc9d3f8dd491b1075
Jonatan Amaya <128762808+jonatanpucp@users.noreply.github.com>
2023-04-07 23:22:36 -0500

    Create Grupo_2_r.R

---
+++ Labs/tarea1/Grupo_2_r.R
@@ -0,0 +1,12 @@
+#Tarea ####
+
+#Paste ####
+anio<-2023
+cadena1<-"Facultad de Ciencias Sociales"
+imprimir <- paste(cadena1,anio, sep = " / ") 
+print(imprimir)
+#Operador pip ####
+numero<--3.1416 %>% abs() 
+numeroCuadrado<-numero^2
+numeroEntero<-numeroCuadrado %>% log() %>% as.integer()
+numeroEntero
