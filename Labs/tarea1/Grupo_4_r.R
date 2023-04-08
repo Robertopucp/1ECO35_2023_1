26733cad2e6cf2f9784221e40737f966c95cec47
Sterben2530 <a20195951@pucp.edu.pe>
2023-04-08 12:50:49 -0500

    #

---
+++ Labs/tarea1/Grupo_4_r.R
@@ -0,0 +1,19 @@
+# Tarea1
+## Integrantes: Mishell Delgado, Lisbeth Ccoyo y Steven Atoche
+
+# Paste
+
+b1 <- "Facultad de Ciencias Sociales"
+b2 <- '\ 2023'
+
+d <- paste0(b1, b2)
+
+print(d)
+
+#Activamos las librerías
+
+library(dplyr) 
+library(stringr)
+
+# operator pip %>% 
+-3.1416 %>% abs() %>% sqrt() %>% log() %>% round()
