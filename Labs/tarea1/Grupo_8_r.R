865ec8223dfff77032bbfe0b45c69aefa1fb5f66
Pamela Obregon <128948502+pameov@users.noreply.github.com>
2023-04-08 21:12:55 -0500

    #1

--- Labs/tarea1/Grupo_8_r.R
+++ Labs/tarea1/Grupo_8_r.R
@@ -1,4 +1,9 @@
 #TAREA 1------------------------------------------------
+#Grupo 8
+# Integrantes:
+#- Renzo Mosquera (20181960)
+#- Yenner Huancahuire (20173340)
+#- Pamela Obregón (20173040)
 
 #Intalacion de librerias: 
 #La instalacion de esta libreria nos permitira hacer el uso del 
