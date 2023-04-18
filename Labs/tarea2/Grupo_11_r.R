328e8c47ffa01ddf2f480030d0547380ab144c16
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-04-18 16:51:48 -0500

    Preguntas_R

---
+++ Labs/tarea2/Grupo_11_r.R
@@ -0,0 +1,29 @@
+# Integrantes ####
+# Kevin Pareja (20196318)
+# Elian Tongombol (20196453)
+# Paola Aranda (20196052)
+# María Alejandra Colán (20190515)
+
+# Consideraciones previas: #####
+## Borrando el environment ####
+rm(list = ls())
+
+## Borrando los gráficos ####
+graphics.off()
+
+##Borrando la consola ####
+cat("\014")
+
+## Llamando a los directorios necesarios
+library(dplyr)
+library(stringr)
+
+# Desarrollo de las preguntas ####
+
+## Pregunta 1: Función #### 
+
+## Pregunta 2: Función de masa corporal #### 
+
+
+## Pregunta 3: Función aplicando activos financieros #### 
+
