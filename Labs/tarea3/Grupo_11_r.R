cccae587b5d37741823b02d73fff5fd0dd1b5fb6
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-04-27 22:50:46 -0500

    Datos_R

---
+++ Labs/tarea3/Grupo_11_r.R
@@ -0,0 +1,27 @@
+# Integrantes ####
+# Kevin Pareja (20196318)
+# Elian Tongombol (20196453)
+# Paola Aranda (20196052)
+# Maria Alejandra Colan (20190515)
+
+# Consideraciones previas: #####
+## Borrando el environment ####
+rm(list = ls())
+
+## Borrando los graficos ####
+graphics.off()
+
+##Borrando la consola ####
+cat("\014")
+
+## Llamando a los directorios necesarios
+library(dplyr)
+library(stringr)
+
+# Desarrollo de las preguntas ####
+
+## Pregunta 1:  ####
+
+## Pregunta 2: Condicional 1 ####
+
+## Pregunta 3: Condicional 1 ####
