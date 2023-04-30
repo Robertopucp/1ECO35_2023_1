489fb36f9b3b7a43f0845396dfc24a983433c9f6
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-04-29 21:55:14 -0500

    avance R

--- Labs/tarea3/Grupo_11_r.R
+++ Labs/tarea3/Grupo_11_r.R
@@ -15,15 +15,38 @@ graphics.off()
 cat("\014")
 
 ## Llamando a los directorios necesarios
-library(dplyr)
-library(stringr)
+library(pacman) 
+p_load(dplyr, readxl, tidyverse, foreign, datos) 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
 
 # Desarrollo de las preguntas ####
 
 ## Pregunta 1: Map, sapply, apply, función Lambda ####
+base_compras <- read.csv("../../data/BDD_compras_consumidores.csv", sep = ";")
+
+vector <- runif(100) # Creamos el vector aletoario
+View(vector)         # Vemos los números aleatoreos creados en un rango de (0 a 1)
+
+lapply(vector, function(x)  ( x -  min(vector) ) / max(vector) - min(vector) )
+
+
+
 
 ## Pregunta 2: Apply ####
 siagie <- read.csv("../../data/siagie.csv")
+str(siagie)
 
+# Hallando el promedio de cada alumno 
+siagie$Promedio <- apply(siagie[, 7:17], 1, mean)
+ 
+# Hallando nota máxima de cada alumno 
+siagie$Nota_max <- apply(siagie[, 7:17], 1, max)
+
+# Hallando nota mínima de cada alumno
+siagie$Nota_max <- apply(siagie[, 7:17], 1, min)
+
+# Hallando el promedio de cada curso
+siagie$Promedio_curso1 <- apply(siagie[, 7:17], 1, mean)
 
 
