2d954e80216eb0fb76f965af23c0521f9e3cc84e
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-04-29 23:41:22 -0500

    Ayuda xdd

--- Labs/tarea3/Grupo_11_r.R
+++ Labs/tarea3/Grupo_11_r.R
@@ -23,8 +23,13 @@ getwd()
 # Desarrollo de las preguntas ####
 
 ## Pregunta 1: Map, sapply, apply, función Lambda ####
+sample(1:100000, 100, replace=FALSE)
 
-vector <- sample(100) # Creamos el vector aletoario
+vector <- runif(100, min=0, max=15000) # Creamos el vector aletoario
+view(vector)
+
+sapply(vector, function(x) (x - min(vector))/(max(vector) - min(vector)))
+
 base_compras <- read.csv("../../data/BDD_compras_consumidores.csv", sep = ";")
 base_compras$escalmamiento <- apply(base_compras[, 3:8], 2, function(x)
   (x - min(vector))/(max(vector)- min(vector)))
@@ -42,6 +47,13 @@ siagie$Promedio <- apply(siagie[, 7:17], 1, mean)
 siagie$Nota_max <- apply(siagie[, 7:17], 1, max)
 
 # Hallando nota mínima de cada alumno
-siagie$Nota_max <- apply(siagie[, 7:17], 1, min)
+siagie$Nota_min <- apply(siagie[, 7:17], 1, min)
 
 # Hallando el promedio de cada curso
+Promedio_cursos <- apply(siagie[, 7:17], 2, mean)
+print (Promedio_cursos)
+
+# Hallando el mediana de cada curso
+Mediana_cursos <- apply(siagie[, 7:17], 2, median)
+print (Mediana_cursos)
+
