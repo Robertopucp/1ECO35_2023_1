9cf22899b729c8e8a8aa3da8656dd41a8ff25a09
yenneraih <yenner.huancahuire@pucp.edu.pe>
2023-04-30 22:38:19 -0500

    #43

--- Labs/tarea3/Grupo_8_py.py
+++ Labs/tarea3/Grupo_8_py.py
@@ -90,3 +90,131 @@ print(NPC)
 NMC = notas.iloc[:,6:17].apply(lambda x: np.median(x), axis = 0)
 print(NMC)
 
+
+
+
+#PARTE 3
+#3.1.- Crear una función que permita calcular el factorial de un número (n).
+
+def factorial(n):
+    x=1
+    if n<0:
+        print('no valido')
+        
+    elif n==0:
+        print(1)
+        
+    elif n>0:
+        for i in range(1,n+1):
+            x=x*i
+   
+        
+    return x
+
+#Por ejemplo 
+
+#si n<0
+factorial(-3)
+
+#si n=0
+factorial(0)
+
+#si n>0
+factorial(7)
+
+
+
+#3.2-FUNCION DE MASA CORPORAL
+#Creamos una funcion que indica el Indice de masa corporal (peso/talla^2)
+
+def BMI(peso,talla):
+    IMC= peso/(talla*talla)
+    
+    if 18.5<=IMC<25:
+        clasificacion="Normal"
+    elif 25<=IMC<30:
+        clasificacion="Sobrepeso"
+    elif 30<=IMC<35:
+        clasificacion="Obesidad grado 1"
+    elif 35<=IMC<=40:
+        clasificacion="Obesidad grado 2"
+    elif 40<IMC:
+        clasificacion="Obesidad grado 3"
+    IMC = "{:.2f}".format(IMC)
+    resultado=(peso,talla,IMC,clasificacion)
+    return resultado
+
+#Pasamos a colocar los datos de los estudiantes propuestos
+
+#Estudiante 1
+
+print("Estudiante 1")
+print(BMI(70, 1.5))
+
+#Estudiante 2
+
+print("Estudiante 2")
+print(BMI(85, 1.8))
+
+#Estudiante 1
+
+print("Estudiante 3")
+print(BMI(50, 1.6))
+
+#3.3-FUNCION APLICADO A DOS ACTIVOS FINANCIEROS
+
+portfolio = pd.read_csv("../../data/Portafolio.csv")
+portfolio
+
+# Extraemos cada columna de datos
+
+x = portfolio['X']
+y = portfolio['Y']
+
+w1= 0.2 #Fijamos el peso del activo "X"
+w2= 0.8 #Fijamos el peso del activo "Y"
+
+# Formula de Coeficiente de correlacion de Pearson
+def coeficiente_pearson(x,y):
+    coeficiente=np.cov(x,y)[0][1]/(math.sqrt(np.var(x, ddof=1)*np.var(y, ddof=1)))
+    return coeficiente
+coeficiente_pearson(x,y)
+
+
+#Determinamos la varianza del portafolio
+
+def varianza(x,y,w1,w2):
+    varianza_p=w1*w1*np.var(x, ddof=1)+w2*w2*np.var(y, ddof=1)+2*w1*w2*np.cov(x,y)[0][1]
+    return varianza_p
+
+varianza(x,y,w1,w2)
+
+
+
+#3.4.- KEYWORDS ESPECIALES
+## Definimos dos métodos que admita la función de escalamiento y transformación
+
+def f_resc_inv(*args, **kwargs):
+    if (kwargs['function']=="reescalamiento"):
+        #Insertamos la función utilizada en la primera sección
+        vector = np.array(list(args))
+        result=reescalamiento_0 = list(map( lambda x: (x - np.min(vector))/(np.max(vector)-np.min(vector)), vector) )
+    elif (kwargs['function']=="transformacion"):
+        x = np.array(list(args))
+        result=np.log(x+np.sqrt(np.square(x)+1))
+    else:
+        raise ValueError( f"The function argument {kwargs[ 'function' ]} is not supported." )
+        
+    return result
+
+#1. Reescalamiento
+A=f_resc_inv(2,5,7,9, function="reescalamiento")
+print(A)
+
+#2.Transformación
+B=f_resc_inv(99,2,0,67,57,34,7,function="transformacion")
+print(B)
+
+#Error
+C=f_resc_inv(1,9, function="suma")
+print(C)
