31abaa1cdc52e9b80bdd9d331d80dac0e071741c
Sterben2530 <a20195951@pucp.edu.pe>
2023-06-11 19:42:27 -0500

    #

---
+++ Labs/tarea6/Grupo_4_r.R
@@ -0,0 +1,68 @@
+
+
+# Grupo_4_r 
+
+rm(list = ls())
+
+library(readxl)
+library(dplyr) 
+library(rstudioapi)  
+library(readr)
+library(haven) 
+library(pacman) 
+library(stringr)
+library(lubridate)
+library(foreign)
+
+
+# clean environment variables
+
+rm(list = ls())
+
+# clean plots
+graphics.off()
+
+# clean console
+
+cat("\014")
+
+############################################################
+
+
+ruta_archivo <- "C:/Users/ASUS/Documents/GitHub/1ECO35_2023_1/data/Juntos_program/data.dta"
+
+data <- haven::read_dta(ruta_archivo)
+colnames(data) <- tolower(colnames(data)) 
+
+colnames(data)
+library(tidyr)
+library(dplyr)
+
+data_long <- reshape(data, idvar=c("numper", "numpanh15"), varying = 7:ncol(data), sep = "_", direction = "long")
+library(dplyr)
+library(tidyverse)
+library(foreign)
+
+data_unidos <- read_dta("C:/Users/ASUS/Documents/GitHub/1ECO35_2023_1/data/Juntos_program/unidos.dta")
+
+merged_data <- merge(data_long, data_unidos, by = "ubigeo")
+
+# para poner etiquetas a las variables p4022 y unidos
+
+if (!require(sjlabelled)) {
+  install.packages("sjlabelled")
+}
+
+library(sjlabelled)
+
+set_label(merged_data$ubigeo) <- "Ubigeo"
+set_label(merged_data$numper) <- "Número de la persona"
+set_label(merged_data$time) <- "Año"
+set_label(merged_data$mes) <- "Mes"
+set_label(merged_data$dominio) <- "Dominio"
+set_label(merged_data$p400a3) <- "P400a3"
+set_label(merged_data$p4022) <- "P4022"
+set_label(merged_data$distrito) <- "Distrito"
+set_label(merged_data$provincia) <- "Provincia"
+set_label(merged_data$region) <- "Región"
+set_label(merged_data$unidos) <- "Unidos"
