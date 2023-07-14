8148cbcc9ca48109f35783d6f6416e63dc41b723
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-07-14 11:59:18 -0500

    Pregunta 2 R

--- Trabajo_final/Grupo11/Regresión Lineal - R.R
+++ Trabajo_final/Grupo11/Regresión Lineal - R.R
@@ -4,8 +4,7 @@
 # Paola Aranda (20196052)
 # Maria Alejandra Colan (20190515)
 
-# Para limpiar el workspace, por si hubiera algún dataset 
-# o información cargada
+# Para limpiar el workspace, por si hubiera algún dataset o información cargada
 rm(list = ls())
 
 # Para limpiar el área de gráficos
@@ -65,8 +64,6 @@ stargazer(table1, title = "Descriptive Statistics", digits = 2, # decimales con 
           , notes.append = FALSE, # TRUE append the significance levels
           notes.align = 'r')
 
-
-
 #------------------------------------------------------------#
 ##### PREGUNTA 2: REPLICANDO TABLAS #####
 #------------------------------------------------------------#
@@ -75,17 +72,209 @@ stargazer(table1, title = "Descriptive Statistics", digits = 2, # decimales con 
 ###### TABLA 3 ###### 
 #_____________________#
 
+#- - - - - - - - - - - - MODELO 1 - - - - - - - - - - - -#
+# NO consideramos variables de control
+# Tenemos la fórmula
+model1_3_formula <- as.formula(paste("took_year","~",
+                                     paste("treat2016","yr_2016","treatment_class", sep = "+")))
+# Realizando la estimación
+ols_model1 <- lm_robust(model1_3_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model1)
+
+
+#- - - - - - - - - - - - MODELO 2 - - - - - - - - - - - -#
+# SI consideramos variables de control)
+# incuyendo las variables de control
+control_vars <- c("female_prof", "instate", "freshman", "american",
+                  "ACumGPA", "gradePrinciples","small_class")
+
+# tenemos la fórmula
+model2_3_formula <- as.formula(paste("took_year","~", paste("treat2016","yr_2016","treatment_class",
+              paste(control_vars, collapse = "+"),sep="+")))
+
+# Estimando los resultados
+ols_model2 <- lm_robust(model2_3_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model2) 
+
+
+#- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
+# NO consideramos variables de control
+# Tenemos la fórmula
+model3_3_formula <- as.formula(paste("tookanother","~",
+                                     paste("treat2016","yr_2016","treatment_class", sep = "+")))
+# Realizando la estimación
+ols_model3 <- lm_robust(model3_3_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model3)
+
+
+#- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
+# NO consideramos variables de control
+# tenemos la fórmula
+model4_3_formula <- as.formula(paste("tookanother","~", paste("treat2016","yr_2016","treatment_class",
+                                                            paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model4 <- lm_robust(model4_3_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model4) 
+
+#- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
+
+
 #_____________________#
 ###### TABLA 4 ###### 
 #_____________________#
 
+#- - - - - - - - - - - - MODELO 1 - - - - - - - - - - - -#
+# NO consideramos variables de control
+model1_4_formula <- as.formula(paste("numeconclass","~",
+                                     paste("treat2016","yr_2016","treatment_class", sep = "+")))
+# Realizando la estimación
+ols_model1 <- lm_robust(model1_4_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model1)
+
+
+#- - - - - - - - - - - - MODELO 2 - - - - - - - - - - - -#
+# NO consideramos variables de control
+# tenemos la fórmula
+model2_4_formula <- as.formula(paste("numeconclass","~", paste("treat2016","yr_2016","treatment_class",
+                                                              paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model2 <- lm_robust(model2_4_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model2) 
+
+
+#- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
+# NO consideramos variables de control
+model3_4_formula <- as.formula(paste("econmajor","~",
+                                     paste("treat2016","yr_2016","treatment_class", sep = "+")))
+# Realizando la estimación
+ols_model3 <- lm_robust(model3_4_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model3)
+
+
+#- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
+# NO consideramos variables de control
+# tenemos la fórmula
+model4_4_formula <- as.formula(paste("econmajor","~", paste("treat2016","yr_2016","treatment_class",
+                                                               paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model4 <- lm_robust(model4_4_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model4) 
+
+#- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
+
+
 #_____________________#
 ###### TABLA 5 ###### 
 #_____________________#
+# Todos los modelos presnetan presentan variables de control
+
+#- - - - - - - - - - - - MODELO 1 - - - - - - - - - - - -#
+model1_5_formula <- as.formula(paste("Major_STEM","~", paste("treat2016","yr_2016","treatment_class",
+                                                            paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model1 <- lm_robust(model1_5_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model1) 
+
+
+#- - - - - - - - - - - - MODELO 2 - - - - - - - - - - - -#
+model2_5_formula <- as.formula(paste("Major_Business","~", paste("treat2016","yr_2016","treatment_class",
+                                                             paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model2 <- lm_robust(model2_5_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model2) 
+
+
+#- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
+model3_5_formula <- as.formula(paste("Major_Finance","~", paste("treat2016","yr_2016","treatment_class",
+                                                                 paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model3 <- lm_robust(model3_5_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model3) 
+
+
+#- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
+model4_5_formula <- as.formula(paste("Major_Marketing","~", paste("treat2016","yr_2016","treatment_class",
+                                                                paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model4 <- lm_robust(model4_5_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model4) 
+
+
+#- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
+
+
 
 #_____________________#
 ###### TABLA 6 ###### 
 #_____________________#
+# Todos los modelos presnetan presentan variables de control
+
+#- - - - - - - - - - - - MODELO 1 - - - - - - - - - - - -#
+model1_6_formula <- as.formula(paste("Major_SocSc","~", paste("treat2016","yr_2016","treatment_class",
+                                                             paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model1 <- lm_robust(model1_6_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model1) 
+
+
+#- - - - - - - - - - - - MODELO 2 - - - - - - - - - - - -#
+model2_6_formula <- as.formula(paste("Major_Arts","~", paste("treat2016","yr_2016","treatment_class",
+                                                                 paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model2 <- lm_robust(model2_6_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model2) 
+
+
+#- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
+model3_6_formula <- as.formula(paste("Major_Comm","~", paste("treat2016","yr_2016","treatment_class",
+                                                                paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model3 <- lm_robust(model3_6_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model3) 
+
+
+#- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
+model4_6_formula <- as.formula(paste("Major_Hum","~", paste("treat2016","yr_2016","treatment_class",
+                                                                  paste(control_vars, collapse = "+"),sep="+")))
+# Estimando los resultados
+ols_model4 <- lm_robust(model4_6_formula, data = datos,
+                        clusters = class_fe2, se_type = "stata")
+# Viendo los resultados
+summary(ols_model4) 
+
+
+#- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
+
 
 #------------------------------------------------------------#
 ##### PREGUNTA 3: CREACIÓN DE UN COEFPLOT DE LA TABLA 4 #####
