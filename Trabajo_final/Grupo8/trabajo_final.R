4157bfc4c657974d9c5470c42106ffd2a949116b
RenzoMosquera <128960051+RenzoMosquera@users.noreply.github.com>
2023-07-14 03:18:26 -0500

    #TrabajoFinal

---
+++ Trabajo_final/Grupo8/trabajo_final.R
@@ -0,0 +1,17 @@
+#TRABAJO FINAL------------------------------------------------
+#Grupo 8
+# Integrantes:
+#- Renzo Mosquera (20181960)
+#- Yenner Huancahuire (20173340)
+#- Pamela Obregón (20173040)
+
+# Clean y opciones adicionales
+
+rm(list = ls())
+graphics.off()
+
+#REGRESIONES LINEALES-----------------------------------------
+
+# Librerías necesarias
+
+options(scipen = 999) 
