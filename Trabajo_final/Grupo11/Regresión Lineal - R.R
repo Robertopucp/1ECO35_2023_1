78d61a6cdccb5dff47feae1bfc6bccb3f2ea251f
Kevin-Pareja <128569634+Kevin-Pareja@users.noreply.github.com>
2023-07-14 21:48:36 -0500

    R final

--- Trabajo_final/Grupo11/Regresión Lineal - R.R
+++ Trabajo_final/Grupo11/Regresión Lineal - R.R
@@ -78,21 +78,16 @@ stargazer(table1, title = "Descriptive Statistics", digits = 2, # decimales con 
 model1_3_formula <- as.formula(paste("took_year","~",
                                      paste("treat2016","yr_2016","treatment_class", sep = "+")))
 model1_3_formula
-# Realizando la estimación
-ols_model1 <- lm_robust(model1_3_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model1) 
 
-# Otra forma de estimacion
-m1 <- lm(model1_3_formula, data = datos)
-m1
+# Estimando
+m1_3 <- lm(model1_3_formula, data = datos)
+m1_3
 
 #Sacar errores estandar robustos
-robust_model1_3 <- coeftest(m1,
-                          vcov = vcovCL,
-                          type = "HC1",
-                          cluster = ~ class_fe2)
+robust_model1_3 <- coeftest(m1_3,
+                            vcov = vcovCL,
+                            type = "HC1",
+                            cluster = ~ class_fe2)
 
 sd_robust_model1_3 <- robust_model1_3[,2] 
 sd_robust_model1_3
@@ -109,18 +104,11 @@ model2_3_formula <- as.formula(paste("took_year","~", paste("treat2016","yr_2016
               paste(control_vars, collapse = "+"),sep="+")))
 model2_3_formula
 # Estimando los resultados
-ols_model2 <- lm_robust(model2_3_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model2) 
-
-# Otra forma de estimación
-m2 <- lm(model2_3_formula, data = datos)
-m2
-
+m2_3 <- lm(model2_3_formula, data = datos)
+m2_3
 
 #Sacar errores estandar robustos
-robust_model2_3 <- coeftest(m2,
+robust_model2_3 <- coeftest(m2_3,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -135,18 +123,13 @@ sd_robust_model2_3
 model3_3_formula <- as.formula(paste("tookanother","~",
                                      paste("treat2016","yr_2016","treatment_class", sep = "+")))
 model3_3_formula
-# Realizando la estimación
-ols_model3 <- lm_robust(model3_3_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model3)
 
-# Otra forma de estimación
-m3 <- lm(model3_3_formula, data = datos)
-m3
+# Realizando la estimación
+m3_3 <- lm(model3_3_formula, data = datos)
+m3_3
 
 #Sacar errores estandar robustos
-robust_model3_3 <- coeftest(m3,
+robust_model3_3 <- coeftest(m3_3,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -160,19 +143,15 @@ sd_robust_model3_3
 # Tenemos la fórmula
 model4_3_formula <- as.formula(paste("tookanother","~", paste("treat2016","yr_2016","treatment_class",
                                                             paste(control_vars, collapse = "+"),sep="+")))
+model4_3_formula
+
 # Estimando los resultados
-ols_model4 <- lm_robust(model4_3_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model4) 
 
-
-# Otra forma de estimación
-m4 <- lm(model4_3_formula, data = datos)
-m4
+m4_3 <- lm(model4_3_formula, data = datos)
+m4_3
 
 #Sacar errores estandar robustos
-robust_model4_3 <- coeftest(m4,
+robust_model4_3 <- coeftest(m4_3,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -183,7 +162,7 @@ sd_robust_model4_3
 
 #- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
 
-stargazer( m1, m2, m3, m4,
+stargazer( m1_3, m2_3, m3_3, m4_3,
            se=list(sd_robust_model1_3, sd_robust_model2_3, sd_robust_model3_3,
                    sd_robust_model4_3),
            dep.var.labels = c("Took Micro within year","Took another econclass"),
@@ -210,17 +189,11 @@ model1_4_formula <- as.formula(paste("numeconclass","~",
 model1_4_formula
 
 # Realizando la estimación
-ols_model1 <- lm_robust(model1_4_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model1)
-
-# Otra forma de estimacion
-m1 <- lm(model1_4_formula, data = datos)
-m1
+m1_4 <- lm(model1_4_formula, data = datos)
+m1_4
 
 #Sacar errores estandar robustos
-robust_model1_4 <- coeftest(m1,
+robust_model1_4 <- coeftest(m1_4,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -237,24 +210,17 @@ model2_4_formula <- as.formula(paste("numeconclass","~", paste("treat2016","yr_2
 model2_4_formula
 
 # Estimando los resultados
-ols_model2 <- lm_robust(model2_4_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model2) 
-
-# Otra forma de estimación
-m2 <- lm(model2_4_formula, data = datos)
-m2
+m2_4 <- lm(model2_4_formula, data = datos)
+m2_4
 
 #Sacar errores estandar robustos
-robust_model2_4 <- coeftest(m2,
+robust_model2_4 <- coeftest(m2_4,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
 
 sd_robust_model2_4 <- robust_model2_4[,2] 
 sd_robust_model2_4
-
 
 
 #- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
@@ -264,17 +230,11 @@ model3_4_formula <- as.formula(paste("econmajor","~",
 model3_4_formula
 
 # Realizando la estimación
-ols_model3 <- lm_robust(model3_4_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model3)
-
-# Otra forma de estimación
-m3 <- lm(model3_4_formula, data = datos)
-m3
+m3_4 <- lm(model3_4_formula, data = datos)
+m3_4
 
 #Sacar errores estandar robustos
-robust_model3_4 <- coeftest(m3,
+robust_model3_4 <- coeftest(m3_4,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -285,22 +245,18 @@ sd_robust_model3_4
 
 #- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
 # NO consideramos variables de control
-# tenemos la fórmula
+# Tenemos la fórmula
 model4_4_formula <- as.formula(paste("econmajor","~", paste("treat2016","yr_2016","treatment_class",
                                                                paste(control_vars, collapse = "+"),sep="+")))
+model4_4_formula
+
 # Estimando los resultados
-ols_model4 <- lm_robust(model4_4_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model4) 
 
-
-# Otra forma de estimación
-m4 <- lm(model4_4_formula, data = datos)
-m4
+m4_4 <- lm(model4_4_formula, data = datos)
+m4_4
 
 #Sacar errores estandar robustos
-robust_model4_4 <- coeftest(m4,
+robust_model4_4 <- coeftest(m4_4,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -311,7 +267,7 @@ sd_robust_model4_4
 
 #- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
 
-stargazer( m1, m2, m3, m4,
+stargazer( m1_4, m2_4, m3_4, m4_4,
            se=list(sd_robust_model1_4, sd_robust_model2_4, sd_robust_model3_4,
                    sd_robust_model4_4),
            dep.var.labels = c("Number of econ classes taken", "Major in economics"),
@@ -339,18 +295,11 @@ model1_5_formula <- as.formula(paste("Major_STEM","~", paste("treat2016","yr_201
 model1_5_formula
 
 # Estmando los resultados
-ols_model1 <- lm_robust(model1_5_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model1) 
-
-
-# Otra forma de estimacion
-m1 <- lm(model1_5_formula, data = datos)
-m1
+m1_5 <- lm(model1_5_formula, data = datos)
+m1_5
 
 #Sacar errores estandar robustos
-robust_model1_5 <- coeftest(m1,
+robust_model1_5 <- coeftest(m1_5,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -365,17 +314,11 @@ model2_5_formula <- as.formula(paste("Major_Business","~", paste("treat2016","yr
 model2_5_formula
 
 # Estimando los resultados
-ols_model2 <- lm_robust(model2_5_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model2) 
-
-# Otra forma de estimación
-m2 <- lm(model2_5_formula, data = datos)
-m2
+m2_5 <- lm(model2_5_formula, data = datos)
+m2_5
 
 #Sacar errores estandar robustos
-robust_model2_5 <- coeftest(m2,
+robust_model2_5 <- coeftest(m2_5,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -384,24 +327,17 @@ sd_robust_model2_5 <- robust_model2_5[,2]
 sd_robust_model2_5
 
 
-
 #- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
 model3_5_formula <- as.formula(paste("Major_Finance","~", paste("treat2016","yr_2016","treatment_class",
                                                                  paste(control_vars, collapse = "+"),sep="+")))
 model3_5_formula
 
 # Estimando los resultados
-ols_model3 <- lm_robust(model3_5_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model3) 
-
-# Otra forma de estimación
-m3 <- lm(model3_5_formula, data = datos)
-m3
+m3_5 <- lm(model3_5_formula, data = datos)
+m3_5
 
 #Sacar errores estandar robustos
-robust_model3_5 <- coeftest(m3,
+robust_model3_5 <- coeftest(m3_5,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -410,24 +346,17 @@ sd_robust_model3_5 <- robust_model3_5[,2]
 sd_robust_model3_5
 
 
-
 #- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
 model4_5_formula <- as.formula(paste("Major_Marketing","~", paste("treat2016","yr_2016","treatment_class",
                                                                 paste(control_vars, collapse = "+"),sep="+")))
 model4_5_formula
 
 # Estimando los resultados
-ols_model4 <- lm_robust(model4_5_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model4) 
-
-# Otra forma de estimación
-m4 <- lm(model4_5_formula, data = datos)
-m4
+m4_5 <- lm(model4_5_formula, data = datos)
+m4_5
 
 #Sacar errores estandar robustos
-robust_model4_5 <- coeftest(m4,
+robust_model4_5 <- coeftest(m4_5,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -438,7 +367,7 @@ sd_robust_model4_5
 
 #- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
 
-stargazer( m1, m2, m3, m4,
+stargazer( m1_5, m2_5, m3_5, m4_5,
            se=list(sd_robust_model1_5, sd_robust_model2_5, sd_robust_model3_5,
                    sd_robust_model4_5),
            dep.var.labels = c("Major STEM","Major busines", "Major finance", "Major marketing"),
@@ -465,21 +394,11 @@ model1_6_formula <- as.formula(paste("Major_SocSc","~", paste("treat2016","yr_20
 model1_6_formula
 
 # Estimando los resultados
-ols_model1 <- lm_robust(model1_6_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model1) 
-
-# Otra forma de estimacion
-m1 <- lm(model1_6_formula, data = datos)
-m1
-
-#RMSE
-lm_rmse1 <- round(RMSE(m1$fitted.values, datos$Major_SocSc ) ,2 )
-lm_rmse1
+m1_6 <- lm(model1_6_formula, data = datos)
+m1_6
 
 #Sacar errores estandar robustos
-robust_model1_6 <- coeftest(m1,
+robust_model1_6 <- coeftest(m1_6,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -494,21 +413,12 @@ model2_6_formula <- as.formula(paste("Major_Arts","~", paste("treat2016","yr_201
 model2_6_formula
 
 # Estimando los resultados
-ols_model2 <- lm_robust(model2_6_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model2) 
+m2_6 <- lm(model2_6_formula, data = datos)
+m2_6
 
-# Otra forma de estimación
-m2 <- lm(model2_6_formula, data = datos)
-m2
-
-#RMSE
-lm_rmse2 <- round(RMSE(m2$fitted.values, datos$Major_Arts ) ,2 )
-lm_rmse2
 
 #Sacar errores estandar robustos
-robust_model2_6 <- coeftest(m2,
+robust_model2_6 <- coeftest(m2_6,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -521,21 +431,11 @@ sd_robust_model2_6
 model3_6_formula <- as.formula(paste("Major_Comm","~", paste("treat2016","yr_2016","treatment_class",
                                                                 paste(control_vars, collapse = "+"),sep="+")))
 # Estimando los resultados
-ols_model3 <- lm_robust(model3_6_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model3) 
-
-# Otra forma de estimación
-m3 <- lm(model3_6_formula, data = datos)
-m3
-
-#RMSE
-lm_rmse3 <- round(RMSE(m3$fitted.values, datos$Major_Comm) ,2 )
-lm_rmse3
+m3_6 <- lm(model3_6_formula, data = datos)
+m3_6
 
 #Sacar errores estandar robustos
-robust_model3_6 <- coeftest(m3,
+robust_model3_6 <- coeftest(m3_6,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -544,28 +444,17 @@ sd_robust_model3_6 <- robust_model3_6[,2]
 sd_robust_model3_6
 
 
-
 #- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
 model4_6_formula <- as.formula(paste("Major_Hum","~", paste("treat2016","yr_2016","treatment_class",
                                                                   paste(control_vars, collapse = "+"),sep="+")))
 model4_6_formula
 
 # Estimando los resultados
-ols_model4 <- lm_robust(model4_6_formula, data = datos,
-                        clusters = class_fe2, se_type = "stata")
-# Viendo los resultados
-summary(ols_model4) 
-
-# Otra forma de estimación
-m4 <- lm(model4_6_formula, data = datos)
-m4
-
-#RMSE
-lm_rmse4 <- round(RMSE(m4$fitted.values, datos$Major_Hum ) ,2 )
-lm_rmse4
+m4_6 <- lm(model4_6_formula, data = datos)
+m4_6
 
 #Sacar errores estandar robustos
-robust_model4_6 <- coeftest(m4,
+robust_model4_6 <- coeftest(m4_6,
                             vcov = vcovCL,
                             type = "HC1",
                             cluster = ~ class_fe2)
@@ -576,7 +465,7 @@ sd_robust_model4_6
 
 #- - - - - - - - - - EXPORTANDO A LATEX - - - - - - - - - -#
 
-stargazer( m1, m2, m3, m4,
+stargazer( m1_6, m2_6, m3_6, m4_6,
            se=list(sd_robust_model1_3, sd_robust_model2_3, sd_robust_model3_3,
                    sd_robust_model4_3),
            dep.var.labels = c("Major social sciences","Major arts",
@@ -597,6 +486,119 @@ stargazer( m1, m2, m3, m4,
 ##### PREGUNTA 3: CREACIÓN DE UN COEFPLOT DE LA TABLA 4 #####
 #------------------------------------------------------------#
 
+#- - - - - - - - - - - - MODELO 1 - - - - - - - - - - - -#
+
+# Extrayendo coeficientes 
+
+model1_4_coef <- robust_model1_4[2,1]
+model1_4_coef
+
+# Desviación estandar
+model1_4_coef_se = robust_model1_4[2,2]
+model1_4_coef_se
+
+# Lower and upper bound
+model1_4_lower = coefci(m1_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,1]
+model1_4_lower
+model1_4_upper = coefci(m1_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,2]
+model1_4_upper
 
 
+#- - - - - - - - - - - - MODELO 2 - - - - - - - - - - - -#
+# Extrayendo coeficientes 
+model2_4_coef <- robust_model2_4[2,1]
+model2_4_coef
 
+# Desviación estandar
+model2_4_coef_se = robust_model2_4[2,2]
+model2_4_coef_se
+
+# Lower and upper bound
+model2_4_lower = coefci(m2_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,1]
+model2_4_lower
+model2_4_upper = coefci(m2_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,2]
+model2_4_upper
+
+#- - - - - - - - - - - - MODELO 3 - - - - - - - - - - - -#
+# Extrayendo coeficientes 
+model3_4_coef <- robust_model3_4[2,1]
+model3_4_coef
+
+# Desviación estandar
+model3_4_coef_se = robust_model3_4[2,2]
+model3_4_coef_se
+
+# Lower and upper bound
+model3_4_lower = coefci(m3_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,1]
+model3_4_lower
+model3_4_upper = coefci(m3_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,2]
+model3_4_upper
+
+#- - - - - - - - - - - - MODELO 4 - - - - - - - - - - - -#
+# Extrayendo coeficientes 
+model4_4_coef <- robust_model4_4[2,1]
+model4_4_coef
+
+# Desviación estandar
+model4_4_coef_se = robust_model4_4[2,2]
+model4_4_coef_se
+
+# Lower and upper bound
+model4_4_lower = coefci(m4_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,1]
+model4_4_lower
+model4_4_upper = coefci(m4_4, df = Inf, vcov. = vcovCL, cluster = ~ class_fe2, type = "HC1")[2,2]
+model4_4_upper
+
+#- - - - - - - - Armanda tabla de resultados - - - - - - - -#
+
+table<- matrix(0, 4, 4)
+table[1,1]<- model1_4_coef
+table[1,2]<- model1_4_coef_se
+table[1,3]<- model1_4_lower
+table[1,4]<- model1_4_upper
+
+table[2,1]<- model2_4_coef
+table[2,2]<- model2_4_coef_se
+table[2,3]<- model2_4_lower
+table[2,4]<- model2_4_upper
+
+table[3,1]<- model3_4_coef
+table[3,2]<- model3_4_coef_se
+table[3,3]<- model3_4_lower
+table[3,4]<- model3_4_upper
+
+table[4,1]<- model4_4_coef
+table[4,2]<- model4_4_coef_se
+table[4,3]<- model4_4_lower
+table[4,4]<- model4_4_upper
+
+colnames(table)<- c("Estimate","Std. Error","Lower_bound","Upper_bound")
+rownames(table)<- c("Model 1", "Model 2", "Model 3", "Model 4")
+
+# Considerar que:
+# Model 1: Number of econ classes taken (No Control)
+# Model 2: Number of econ classes taken (Yes Control)
+# Model 3: Major in economics (No Control)
+# Model 4: Major in economics (Yes Control)
+
+# Exportación a Latex
+xtable(table)
+
+# La tabla se convierte en un data frame 
+tab <- as.data.frame(table)
+tab
+
+# Dimensiones del gráfico 
+options(repr.plot.width = 8, repr.plot.height =5)
+
+
+# Graficando 
+tab  %>% ggplot(aes(x=rownames(tab), y=Estimate)) +
+  geom_point(size=2, color = 'black') +
+  geom_errorbar(aes(ymin=Lower_bound, ymax=Upper_bound) , width = 0.05,color="darkblue", linewidth = 0.8) +
+  labs(x="", y="") + ggtitle("Treatment class x 2016")  +
+  theme(text=element_text(size =15), plot.title = element_text(hjust = 0.5)) +
+  geom_hline(yintercept=0, linetype="dashed", color = "black", size=0.8) +
+  scale_x_discrete(limits = c("Model 1", "Model 2", "Model 3", "Model 4")) +
+  scale_y_continuous(breaks = seq(-0.2,0.2,0.1) , limits = c(-0.2, 1.2)) +
+  theme_classic(14)
